d4bcc9abac0b1ac2418e46da2fbe1014f9567faf
User <huseynovali612@gmail.com>
2024-03-31 22:49:25 +0400

    create user mapper

---
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.example.demo.mapper;
+
+import com.example.demo.dto.request.UserRequestDto;
+import com.example.demo.module.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface UserMapper {
+     User dtoToUser(UserRequestDto userRequestDto);
+
+
+
+}
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.request.UserRequestDto;
+import com.example.demo.mapper.UserMapper;
 import com.example.demo.module.User;
 import com.example.demo.repos.UserRepo;
 import org.springframework.core.SpringVersion;
@@ -10,16 +11,16 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private UserRepo userRepo;
+    private UserMapper mapstructMapper;
 
     public UserService(UserRepo userRepo) {
         this.userRepo = userRepo;
     }
 
     public void addNewUser(UserRequestDto newuser) {
-     userRepo.save(newuser);
+        User user = mapstructMapper.dtoToUser(newuser);
+        userRepo.save(user);
     }
-
-
 
 
 }
